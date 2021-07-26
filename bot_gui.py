f1e9221694ee315aca758ac8cb071f79769d7573
Russell <russellcorbett11@gmail.com>
2021-07-26 09:50:28 -0230

    This is the first commit! Start

--- bot_gui.py
+++ bot_gui.py
@@ -1,1 +1,31 @@
+import tkinter as tk
 
+
+def gui():
+    window = tk.Tk()
+    window.title("OD - 4D ROV Interface")
+    window.geometry("1280x800+0+0")
+    filename = tk.PhotoImage(file="BattleboxBack4.png")
+    background_label = tk.Label(image=filename)
+    background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    # Exit Code Button
+    button = tk.Button(text="QUIT",
+                       activeforeground="Red",
+                       activebackground="Blue",
+                       highlightbackground="Black",
+                       command=exit,
+                       height=8,
+                       width=7).place(x=23, y=30)
+
+    H1 = tk.Label(bg="red", height=8, width=4).place(x=780, y=632)
+    H2 = tk.Label(bg="red", height=8, width=4).place(x=840, y=632)
+    H3 = tk.Label(bg="red", height=8, width=4).place(x=900, y=632)
+    H4 = tk.Label(bg="red", height=8, width=4).place(x=960, y=632)
+    V1 = tk.Label(bg="red", height=8, width=4).place(x=1020, y=632)
+    V2 = tk.Label(bg="red", height=8, width=4).place(x=1080, y=632)
+    V3 = tk.Label(bg="red", height=8, width=4).place(x=1140, y=632)
+    V4 = tk.Label(bg="red", height=8, width=4).place(x=1200, y=632)
+    window.show()
+
+gui()
--- main.py
+++ main.py
@@ -23,9 +23,6 @@ import asyncio
 import evdev
 from evdev import InputDevice, categorize, ecodes
 
-import tkinter as tk
-from pathlib2 import Path
-
 global timer
 global runtime
 global end
@@ -997,36 +994,6 @@ def cameras():
     while not end:
         cv2.imshow('Top_View', top_view)
         cv2.imshow('Bottom_View', bottom_view)
-
-
-'''
-def gui():
-    window = tk.Tk()
-    window.title("OD - 4D ROV Interface")
-    window.geometry("1280x800+0+0")
-    filename = tk.PhotoImage(file="BattleboxBack4.png")
-    background_label = tk.Label(image=filename)
-    background_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-    # Exit Code Button
-    button = tk.Button(text="QUIT",
-                       activeforeground="Red",
-                       activebackground="Blue",
-                       highlightbackground="Black",
-                       command=exit,
-                       height=8,
-                       width=7).place(x=23, y=30)
-
-    H1 = tk.Label(bg="red", height=8, width=4).place(x=780, y=632)
-    H2 = tk.Label(bg="red", height=8, width=4).place(x=840, y=632)
-    H3 = tk.Label(bg="red", height=8, width=4).place(x=900, y=632)
-    H4 = tk.Label(bg="red", height=8, width=4).place(x=960, y=632)
-    V1 = tk.Label(bg="red", height=8, width=4).place(x=1020, y=632)
-    V2 = tk.Label(bg="red", height=8, width=4).place(x=1080, y=632)
-    V3 = tk.Label(bg="red", height=8, width=4).place(x=1140, y=632)
-    V4 = tk.Label(bg="red", height=8, width=4).place(x=1200, y=632)
-    window.show()
-'''
 
 
 def main():
